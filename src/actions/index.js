7012dc1344daa1b9a3b739884e9be1ffacadbaca
J A <jarata@gmail.com>
2019-02-06 13:56:17 -0800

    fixing actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,9 +2,9 @@
 import axios from 'axios';
 // we'll need to create 3 different action types here.
 // one for fetching, one for success and one for failure
-export const FETCH_SW_START;
-export const FETCH_SW_SUCCESS;
-export const FETCH_SW_ERROR;
+export const FETCH_SW_START = 'FETCH_SW_START';
+export const FETCH_SW_SUCCESS = 'FETCH_SW_SUCCESS';
+export const FETCH_SW_ERROR = 'FETCH_SW_ERROR';
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
 // remember that now we have control over our thunk-based action creator
